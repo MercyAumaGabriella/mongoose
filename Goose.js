13b5b0f1c1d8a7bddc869b479d8d5b8a446a3c73
Mercy Auma <aumagabriella@gmail.com>
2021-10-20 10:13:58 +0300

    Introducing mongoose

---
+++ Goose.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+async function main() {
+    //always specify the database eg employees
+    await mongoose.connect("mongodb://localhost:27017/employees");
+
+    //specify a schema
+    const ContactSchema = mongoose.Schema({
+        name: String,
+        phone: String
+    });
+
+    //creating instance of model based on schema. Model links schema to db
+    const ContactModel = mongoose.model("Contacts", ContactSchema);
+
+    //always await the database record retrieval
+    const contacts = await ContactModel.find();
+    contacts.forEach((contact) => {
+        console.log(`The name is: ${contact.name}`);
+    });
+
+    console.log("We are connected");
+};
+
+main();
+
+//mongoose should be used a certain way
